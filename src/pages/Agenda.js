ada02bcf77b7f49c7389e0965c2d21662055be46
emilioCode <emilio_mem@hotmail.com>
2022-07-08 17:44:24 -0400

    Contact module in the Agenda page

--- src/pages/Agenda.js
+++ src/pages/Agenda.js
@@ -1,37 +1,69 @@
-import { Table } from "reactstrap";
+import { Button, Table } from "reactstrap";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Agenda = () => {
+  const [data, setData] = useState([]);
+  const getContacts = async () => {
+    await axios
+      .get("https://localhost:7291/api/Contact")
+      .then((res) => {
+        if (res.data.success) {
+          setData(res.data.data);
+        } else {
+          alert(res.data.message);
+        }
+      })
+      .catch((error) => console.log(error));
+    // .finally((_) => {
+    //   setTimeout(() => {
+    //     setLoading(true);
+    //   }, 1000);
+    // });
+  };
+
+  useEffect(() => {
+    getContacts();
+  }, []);
+
   return (
     <div>
-      <h1>Agenda</h1>
+      <h1>Contact module</h1>
+      <br />
+      <Button color="primary" size="sm" outline>
+        Add new contact
+      </Button>
       <br />
       <Table borderless hover responsive striped>
         <thead>
           <tr>
             <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Contact Name</th>
+            <th>Phone Number</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {data.map((e, index) => (
+            <tr key={index}>
+              <th scope="row">{e.id}</th>
+              <td>{e.nombre}</td>
+              <td>{e.telefono}</td>
+              <td>
+                <Button color="info" size="sm" outline>
+                  Details
+                </Button>
+                &nbsp;
+                <Button color="success" size="sm" outline>
+                  Edit
+                </Button>
+                &nbsp;
+                <Button color="danger" size="sm" outline>
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
